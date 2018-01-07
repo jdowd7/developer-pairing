cb06d4914e518b2c8b3bdeb07124c19db3e41b30
Jake <Jake@JakesMBP.local>
2018-01-06 19:35:31 -0700

    refactor: removed mdBootstrap installed material

--- frontend/ngx-interview-web/src/app/app.module.ts
+++ frontend/ngx-interview-web/src/app/app.module.ts
@@ -3,8 +3,7 @@ import { AssetsService } from './services/assets.service';
 import { CommonService } from './services/common.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule, JsonpModule } from '@angular/http';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DisplayComponentComponent } from './display-component/display-component.component';
@@ -15,12 +14,10 @@ import { DisplayComponentComponent } from './display-component/display-component
     DisplayComponentComponent
   ],
   imports: [
-    MDBBootstrapModule.forRoot(),
     BrowserModule,
     HttpModule,
     JsonpModule
   ],
-  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [
     AssetsService,
     AssetFieldsService,
